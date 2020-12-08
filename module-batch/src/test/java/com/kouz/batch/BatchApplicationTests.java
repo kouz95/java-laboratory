519ffaf9950d9ac678d9620fe509fb0e5f774f22
kouz <lastpuzly@gmail.com>
2020-12-08 23:00:28 +0900

    chore: create batch module

---
+++ module-batch/src/test/java/com/kouz/batch/BatchApplicationTests.java
@@ -0,0 +1,12 @@
+package com.kouz.batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
